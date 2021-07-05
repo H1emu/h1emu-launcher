9c44f2930099bce0ea66b0fc8354f24f76d8e187
Quentin Gruber <quentingruber@gmail.com>
2021-07-05 21:40:58 +0200

    Update MainWindow.xaml.cs

--- H1EMU-Launcher/MainWindow.xaml.cs
+++ H1EMU-Launcher/MainWindow.xaml.cs
@@ -125,7 +125,7 @@ namespace H1EMU_Launcher
         {
             Process.Start(new ProcessStartInfo
             {
-                FileName = "https://github.com/H1emu/h1emu-launcher/releases/latest/download/H1Emu.Launcher.zip",
+                FileName = "https://github.com/H1emu/h1emu-launcher/releases/latest/download/H1EMU-Launcher.zip",
                 UseShellExecute = true
             });
 
